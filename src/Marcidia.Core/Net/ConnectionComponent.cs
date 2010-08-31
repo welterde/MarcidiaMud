f56a892518150f59e24ccf3670794598daa6d8b2
Nik Radford <nik@terminaldischarge.net>
2010-08-31 20:53:43 +0100

    Started work on the Network Subsystem

---
+++ src/Marcidia.Core/Net/ConnectionComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Marcidia.ComponentModel;
+using Marcidia;
+
+namespace Marcidia.Net
+{
+    [MarcidiaComponent(
+        "Network and Connections Subsystem", false, 
+        Description="The core component of the Network and Connection sub system")]
+    public class ConnectionComponent : MarcidiaComponent
+    {
+        public ConnectionComponent(Mud mud)
+            : base(mud)
+        {
+            
+        }
+
+        public override void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Marcidia.Core/Net/IConnectionSource.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Marcidia.Net
+{
+    public interface IConnectionSource : IDisposable
+    {
+        event EventHandler<ConnectionEventArgs> NewConnection;
+        void Start();
+        void Stop();
+    }
+
+    public class ConnectionEventArgs : EventArgs
+    {
+        public ConnectionEventArgs(IConnection connection)
+        {            
+            if (connection == null)
+                throw new ArgumentNullException("connection", "connection is null.");
+
+            Connection = connection;
+        }
+
+        public IConnection Connection { get; set; }
+    }
+}
