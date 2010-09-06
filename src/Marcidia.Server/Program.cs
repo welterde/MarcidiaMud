a688cad7c4d85118c325e9f538db3457aa8e5114
Nik Radford <nik@terminaldischarge.net>
2010-09-06 14:17:53 +0100

    Updated to .NET 4

--- src/Marcidia.Server/Program.cs
+++ src/Marcidia.Server/Program.cs
@@ -7,6 +7,8 @@ using System.IO;
 using System.Threading;
 using Marcidia.ComponentLoading;
 using Marcidia.Net;
+using Marcidia.ComponentModel;
+using Marcidia;
 
 namespace Marcidia.Server
 {
@@ -19,6 +21,77 @@ namespace Marcidia.Server
             mud.Run();
         }
     }
+
+    class CommandLineComponent : MarcidiaComponent, IDisposable
+    {
+        Thread listenThread;
+
+        public CommandLineComponent(Mud mud)
+            : base(mud)
+        {
+            
+        }
+                            
+        public override void Initialize()
+        {
+            listenThread = new Thread(DoConsoleReading);
+            listenThread.Start();
+        }
+
+        private void DoConsoleReading()
+        {
+            try
+            {
+                while (true)
+                {
+                    Console.Write(">");
+                    string read = Console.In.ReadLine();
+
+                    if (read == "components")
+                    {
+                        ListComponents();
+                    }
+                    else if (read == "shutdown")
+                    {
+                        Mud.Shutdown();
+                    }
+                }
+            }
+            catch (ThreadInterruptedException)
+            {
+                // Do nothing, as we expect this
+            }
+        }
+
+        public void Dispose()
+        {
+            if (listenThread != null && listenThread.IsAlive)
+            {
+                listenThread.Interrupt();
+
+                if (Thread.CurrentThread != listenThread)
+                    listenThread.Join();
+            }
+        }
+
+        private void ListComponents()
+        {
+            foreach (var component in Mud.Components)
+            {
+                var componentInfo = ComponentInfo.GetInformationFor(component);
+
+                if (componentInfo == null)
+                {
+                    Console.WriteLine(component.GetType().Name);
+                }
+                else
+                {
+                    Console.WriteLine("{0} {1} by {2}", componentInfo.Name, componentInfo.Version, componentInfo.Author);
+                }
+            }
+        }
+    }
+
 
     class MarcidiaMud : Mud
     {
@@ -34,6 +107,9 @@ namespace Marcidia.Server
 
             ConnectionComponent connectionComponent = new ConnectionComponent(this);
             Components.Add(connectionComponent);
+
+            CommandLineComponent cmdLineComponent = new CommandLineComponent(this);
+            Components.Add(cmdLineComponent);
         }
 
         protected override void Initialize()
