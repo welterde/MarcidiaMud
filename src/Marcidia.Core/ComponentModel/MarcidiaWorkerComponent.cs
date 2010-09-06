7f484dbd079535c447e8edaf4c6f2ece21236373
Nik Radford <nik@terminaldischarge.net>
2010-09-06 14:46:38 +0100

    Added MarcidiaWorkerComponent
    The MarcidiaWorkerComponent is there in anticipation of components that will require a seperate thread to do their work.
    
    This uses a standard pattern of Initialize to start the thread and Dispose to kill it and does some extra safe testing around not joining itself.

---
+++ src/Marcidia.Core/ComponentModel/MarcidiaWorkerComponent.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Marcidia.ComponentModel
+{
+    /// <summary>
+    /// Base class for components that do repeatable work on a secondary thread.
+    /// </summary>    
+    public abstract class MarcidiaWorkerComponent : MarcidiaComponent, IDisposable
+    {
+        bool running;
+        Thread workerThread;
+        
+        public MarcidiaWorkerComponent(Mud mud)
+            : base(mud)
+        {
+            workerThread = new Thread(DoWork);
+        }
+        
+        public override void Initialize()
+        {
+            workerThread.Start();
+        }
+
+        public void Dispose()
+        {
+            running = false;
+
+            if (workerThread != null && workerThread.IsAlive)
+            {
+                workerThread.Interrupt();
+
+                if (workerThread != Thread.CurrentThread)
+                    workerThread.Join();
+            }
+        }
+
+        private void DoThreadLoop()
+        {
+            try
+            {
+                while (running)
+                {
+                    DoWork();
+                }
+            }
+            catch (ThreadInterruptedException)
+            {
+                // We expect this
+            }
+        }
+
+        /// <summary>
+        /// Does the repeatable work of the <see cref="MarcidiaWorkerComponent"/>
+        /// </summary>
+        /// <remarks>
+        /// When inheriting from this class, if you catch Exception rather 
+        /// than catch specific exceptions make sure you rethrow ThreadInterruptedException if they are caught
+        /// or you may cause the Mud server to halt on shutdown.
+        /// </remarks>
+        protected abstract void DoWork();
+    }
+}
