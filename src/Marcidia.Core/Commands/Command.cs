601110de783725e62f33e1e58e5c77e4acd5fe31
Nik Radford <nik@terminaldischarge.net>
2010-09-29 16:19:26 +0100

    Initial Command base class and CommandArguments class written
    CommandArguments parses a command line string it's given into it's components. Using the standard split on whitespace unless contained in quotes.
    
    However the parsing is done in the constructor right now. I don't like it so I'm going to move it into a factory static method I think.
    
    Command is a simple base class, with an Execute() method, derived commands will have to specify a type for the class using generics. This is to set what that command acts on / with. For example some commands may work with the Session object instead of a Player object. This will be reconciled when I get down to creating the CommandManager class and CommandContext classes.
    
    Command's are also constructed with IServiceProvider, this will be because most commands will probably need to access mud services in order to perform their function.

---
+++ src/Marcidia.Core/Commands/Command.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Marcidia.Commands
+{
+
+    public abstract class Command<T>
+    {
+        public IServiceProvider Services { get; private set; }
+        public T Actor { get; private set; }        
+        public CommandArguments Arguments { get; private set; }
+
+        public Command(T actor, CommandArguments arguments, IServiceProvider services)
+        {
+            if (actor == null)
+                throw new ArgumentNullException("actor", "actor is null.");
+            if (arguments == null)
+                throw new ArgumentNullException("arguments", "arguments is null.");
+            if (services == null)
+                throw new ArgumentNullException("services", "services is null.");
+
+            Services = services;
+            Actor = actor;
+            Arguments = arguments;
+        }
+
+        public abstract void Execute();
+    }
+}
---
+++ src/Marcidia.Core/Commands/CommandArguments.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Marcidia.Commands
+{
+    public class CommandArguments
+    {
+        const char EOSMarker = '\0';
+        const int BOS = -1;
+
+        int position;
+        StringReader stringReader;
+        char current;
+
+        public CommandArguments(string commandLine)
+        {
+            position = BOS;
+            current = EOSMarker;
+            
+            CommandLine = commandLine;
+            
+            stringReader = new StringReader(commandLine);
+
+            ParseCommandLine();
+        }
+
+        public string CommandName { get; private set; }
+
+        public string CommandLine { get; private set; }
+
+        public string ArgumentLine { get; private set; }
+
+        public string[] Arguments { get; private set; }
+
+        private char GetNext()
+        {
+            int character = stringReader.Read();
+
+            char c = EOSMarker;
+
+            if (character != -1)
+            {
+                c = (char)character;
+            }
+
+            current = c;
+
+            position++;
+
+            return c;
+        }
+
+        private char GetCurrent()
+        {
+            if (position == BOS)
+                return GetNext();
+
+            return current;
+        }
+
+        private void ParseCommandLine()
+        {
+            CommandName = ReadCommandLineArgument();
+            SkipWhiteSpace();
+            ArgumentLine = CommandLine.Substring(position);
+
+            List<string> arguments = new List<string>();
+
+            while (GetCurrent() != EOSMarker)
+            {
+                arguments.Add(ReadCommandLineArgument());
+            }
+
+            Arguments = arguments.ToArray();
+        }
+
+        private string ReadCommandLineArgument()
+        {            
+            SkipWhiteSpace();
+
+            char c = GetCurrent();
+
+            if (c == '"' || c == '\'')
+            {
+                Func<char, bool> seperatorPredicate = character => character == c;                
+                
+                // skip the seperator start
+                GetNext();
+                
+                return ReadCommandLineWithSeperator(seperatorPredicate);
+            }
+            else
+                return ReadCommandLineWithSeperator(char.IsWhiteSpace);
+
+        }
+
+        private string ReadCommandLineWithSeperator(Func<char, bool> seperatorPredicate)
+        {
+            StringBuilder argumentBuilder = new StringBuilder();
+
+            char c = GetCurrent();
+
+            while (!seperatorPredicate(c) && c != EOSMarker)
+            {
+                argumentBuilder.Append(c);
+                c = GetNext();
+            }
+
+            // Skip seperator character, we know it's worthless
+            GetNext();
+
+            return argumentBuilder.ToString();
+        }        
+
+        private void SkipWhiteSpace()
+        {
+            char c = GetCurrent();
+
+            while (char.IsWhiteSpace(c))
+            {
+                c = GetNext();
+            }
+        }
+    }
+}
