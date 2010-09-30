4a8fb6d901b2918f02df0897188e563de1afd8b3
Nik Radford <nik@terminaldischarge.net>
2010-09-30 13:26:13 +0100

    Added the Admin splash screen session state

---
+++ src/Marcidia.Game.Admin/SessionStates/AdminSplashScreenState.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Marcidia.Sessions;
+using System.IO;
+
+namespace Marcidia.Game.Admin.SessionStates
+{
+    public class AdminSplashScreenState : SessionState
+    {
+        public const string SplashScreenFile = @"Data\AdminSplashScreen.txt";
+
+        // Monostate string in this case to avoid loading it more than once
+        private static string SplashScreenText;
+        
+        protected override void OnInputRecieved(string input)
+        {
+            
+        }
+
+        public override void Start()
+        {
+            LoadSplashScreen();
+
+            Output.WriteLine(SplashScreenText);
+        }
+
+        public override void Stop()
+        {        
+        }
+
+        public override void Resume()
+        {
+            Start();
+        }
+
+        public override void Pause()
+        {         
+        }
+
+        private void LoadSplashScreen()
+        {
+            if (SplashScreenText != null)
+                return;
+
+            using (StreamReader streamReader = new StreamReader(SplashScreenFile))
+            {
+                SplashScreenText = streamReader.ReadToEnd();
+            }
+        }
+    }
+}
